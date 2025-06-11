aecd818fd62940d37f7b23fd5f3f08a95f97cc2f
juhyg-hash <gusrb1474@gmail.com>
2025-06-11 13:35:19 +0900

    fourth commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,25 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("\"안녕하세요?\"");
-        System.out.println("안녕\n사과");
-        System.out.println("안녕\t사과");
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.printf("줄바꿈 가능?");
-        System.out.printf("아니오");
-        System.out.printf("줄바꿈 가능?");
-        System.out.printf("\n아니오");
+////        String name = scanner.nextLine();
+//
+//
+//        System.out.println("입력값 "+name);
+
+//        int age = scanner.nextInt();
+//        System.out.println("내나이는 " + age);
+//
+//        String fullname = scanner.next();
+//        System.out.println(fullname);
+
+        char arr = scanner.next().charAt(1);
+        System.out.println("이걸 찾고싶니? "+ arr);
+
+
+        System.out.println("프로그램 종료");
+
+
     }
 }
