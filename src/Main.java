010176543840ef41eea225b3a3f92b688e55b83b
juhyg-hash <gusrb1474@gmail.com>
2025-06-11 10:41:58 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("나는 행복합니다");
+        System.out.print("프린트함수와");
+        System.out.println("프린트 엘엔 함수의");
+        System.out.print("차이점을 찾아서");
+
+
+
+        String name = "Son";
+        int backNum = 7;
+
+        System.out.println((name + backNum).getClass().getClass());     //타입 확인
+    }
+}
