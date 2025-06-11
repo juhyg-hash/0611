6a6f6057199b4acecf2c59cabe3b736aaa680d1b
juhyg-hash <gusrb1474@gmail.com>
2025-06-11 14:03:59 +0900

    fifth commit

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,37 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-////        String name = scanner.nextLine();
-//
-//
-//        System.out.println("입력값 "+name);
-
-//        int age = scanner.nextInt();
-//        System.out.println("내나이는 " + age);
-//
-//        String fullname = scanner.next();
-//        System.out.println(fullname);
-
-        char arr = scanner.next().charAt(1);
-        System.out.println("이걸 찾고싶니? "+ arr);
+        System.out.println("1번부터 4번까지만 고르세요");
 
 
-        System.out.println("프로그램 종료");
+        int choice = scanner.nextInt();
+        String item = "";
 
+        //입력한 값에 따른 조건문
+        //if문 숫자를 비교정도에만
+        switch (choice) {
+            case 1 :
+                item = "멜론소다";
+                break;
+
+            case 2 :
+                item = "제로콕";
+                break;
+
+            case 3 :
+                item = "탄샨소";
+                break;
+
+            case 4 :
+                item = "포카리";
+                break;
+
+            default :
+                System.out.println("다시고르시오");
+        }
+        System.out.println(item);
 
     }
 }
