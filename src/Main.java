3cfe36efb08c2d61f9a3d55b246fb73fd291970e
juhyg-hash <gusrb1474@gmail.com>
2025-06-12 09:02:58 +0900

    seventh commit

--- src/Main.java
+++ src/Main.java
@@ -4,65 +4,61 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("=== 자판기 메뉴 ===");
-        System.out.println("1. 멜론소다 (2000)");
-        System.out.println("2. 제로 (2200)");
-        System.out.println("3. 탄산수 (1000)");
-        System.out.println("4. 포카리 (2300)");
-        System.out.println();
-        System.out.println("1번부터 4번까지만 고르세요");
+        System.out.println("===계산기입니다.===");
+        System.out.print("첫번째 숫자를 적어주세요 : ");
+        double num1 = scanner.nextDouble();
 
-        int price = 0;
-        int choice = scanner.nextInt();
-        String item = "";
+        System.out.print("두번째 숫자를 적어주세요 : ");
+        double num2 = scanner.nextDouble();
 
-        //입력한 값에 따른 조건문
-        //if문 숫자를 비교정도에만
-        switch (choice) {
-            case 1 :
-                item = "멜론소다";
-                price = 2000;
+        System.out.println("원하시는 기능을 골라주세요. ");
+        System.out.println("+ , - , * , / , % , ^ ");
+
+        char choice = scanner.next().charAt(0);
+        double result;
+
+        switch (choice){
+            case '+':
+                result = num1 + num2;
                 break;
-
-            case 2 :
-                item = "제로콕";
-                price = 2200;
+            case '-':
+                result = num1 - num2;
                 break;
-
-            case 3 :
-                item = "탄산수";
-                price = 1000;
+            case '*':
+                result = num1 * num2;
                 break;
-
-            case 4 :
-                item = "포카리";
-                price = 2300;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("0으로 나눌 수 없습니다.");
+                    return;
+                } else {
+                    result = num1 / num2;
+                }
                 break;
-
-            default :
-                System.out.println("다시고르시오");
-//                break;
-                //return main 함수 를 아예 빠져나감
+            case '%':
+                result = num1 % num2;
+                break;
+            case '^':
+                if (num2 % 1 != 0) {
+                    System.out.println("실수로 제곱할 수 없습니다.");
+                    return;
+                } else {
+                    result = 1;
+                    double square = num2;
+                    while (square > 0){
+                        result = result * num1;
+                        square--;
+                    }
+                }
+                break;
+            default:
+                System.out.println("잘못된 입력입니다.");
                 return;
-
         }
-        System.out.println(item + "을 선택함 금액을 넣어"); //리턴시에는 실행안됨
-        int money = scanner.nextInt();
-
-        //money 와 price 를 비교하는데는 if
-        if(money < price){
-            //돈더넣어야지
-            System.out.println("금액부족 부족한 돈을 추가 넣어"); //while 문으로 변환하면 가능
+        if(num1%1==0 && num2%1==0 && result%1==0){
+            System.out.printf("%d %c %d = %d 입니다", (int)num1, choice, (int)num2, (int)result);
         } else {
-            int change = money - price;
-//            System.out.println(item + "을 드립니다. 잔돈은 "+ change);
-            System.out.printf("%s을 드립니다. 잔돈은 %d입니다.\n",item,change);
+            System.out.printf("%f %c %f = %f 입니다", num1, choice, num2, result);
         }
-
-        System.out.println("잘가");
-
-
-
-
     }
 }
